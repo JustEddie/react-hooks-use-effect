19643cd62dc66ae6e098335433b7ce0d84e8ad2a
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-26 00:40:42 +1000

    useEffect practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name,setName] = useState("");
+
+  // only when count rendering
   useEffect(() => {
     console.log("useEffect called");
-  });
+  },[count]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  function handleCountUpdate() {
+    setCount(count + 1);
+  }
+
+  function handleInputChange(event){
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <button onClick={handleCountUpdate}>Click Me</button>
+      <span>Count: {count}</span>
+      <input type="text" value={name} onChange={handleInputChange}></input>
+      <span>{name}</span>
+
+    </div>
+  );
 }
 
 export default App;
